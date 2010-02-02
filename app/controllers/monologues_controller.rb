8347fb6f8051aa625a238e480c8d3dfce73a73d5
Brandon Faloona <brandon@faloona.net>
2010-02-02 15:24:23 -0800

    updated filter

--- app/controllers/monologues_controller.rb
+++ app/controllers/monologues_controller.rb
@@ -48,7 +48,29 @@ class MonologuesController < ApplicationController
   def search
     #require 'ruby-debug';debugger
     unless params[:search].blank?
-      @monologues = Monologue.find(:all, :conditions => "name like '%#{params[:search]}%'")
+      terms = params[:search].split(" ")
+      @name_monologues = []
+      @body_monologues = []
+      @character_monologues = []
+      terms.each do |term|
+        @name_monologues += Monologue.find(:all, :conditions => "name like '%#{term}%'")
+        @body_monologues += Monologue.find(:all, :conditions => "body like '%#{term}%'")
+        @character_monologues += Monologue.find(:all, :conditions => "character like '%#{term}%'")
+      end
+      # OR
+      @monologues = @name_monologues.flatten + @character_monologues.flatten + @body_monologues.flatten
+      # AND
+#      if !@name_monologues.blank? and !@character_monologues.blank? and !@body_monologues.blank?
+#        @monologues = @name_monologues & @character_monologues & @body_monologues
+#      elsif !@character_monologues.blank? and !@body_monologues.blank?
+#        @monologues = @character_monologues & @body_monologues
+#      elsif !@name_monologues.blank? and !@body_monologues.blank?
+#        @monologues = @name_monologues & @body_monologues
+#      elsif !@name_monologues.blank? and !@character_monologues.blank?
+#        @monologues = @name_monologues & @character_monologues
+#      end
+      @monologues.compact!
+      @monologues.uniq!
     else
       @monologues = Monologue.all
     end
