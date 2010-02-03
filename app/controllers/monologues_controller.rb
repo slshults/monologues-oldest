4109ed65502bed9b9238e4ed43a9382323476c53
Brandon Faloona <brandon@faloona.net>
2010-02-02 19:37:09 -0800

    updated views

--- app/controllers/monologues_controller.rb
+++ app/controllers/monologues_controller.rb
@@ -46,47 +46,14 @@ class MonologuesController < ApplicationController
   end
 
   def search
-    #require 'ruby-debug';debugger
     unless params[:search].blank?
       @terms = params[:search].split(" ")
-
       @monologues = []
       @terms.each do |term|
         results = Monologue.find(:all, :conditions => "character like '%#{term}%' or body like '%#{term}%' or name like '%#{term}%'")
         @monologues = results if results and @monologues.empty?
         @monologues &= results if results
       end
-#      @name_monologues = []
-#      @body_monologues = []
-#      @character_monologues = []
-#      @terms.each do |term|
-#        @name_monologues += Monologue.find(:all, :conditions => "name like '%#{term}%'")
-#        @body_monologues += Monologue.find(:all, :conditions => "body like '%#{term}%'")
-#        @character_monologues += Monologue.find(:all, :conditions => "character like '%#{term}%'")
-#      end
-      # OR
-#      collections = []
-#      collections << @name_monologues.flatten if !@name_monologues.blank?
-#      collections << @character_monologues.flatten if !@character_monologues.blank?
-#      collections << @body_monologues.flatten if !@body_monologues.blank?
-#      case collections.size
-#      when 1
-#        @monologues = collections[0]
-#      when 2
-#        @monologues = collections[0] & collections[1]
-#      when 3
-#        @monologues = collections[0] & collections[1] & collections[2]
-#      end
-      # AND
-#      if !@name_monologues.blank? and !@character_monologues.blank? and !@body_monologues.blank?
-#        @monologues = @name_monologues & @character_monologues & @body_monologues
-#      elsif !@character_monologues.blank? and !@body_monologues.blank?
-#        @monologues = @character_monologues & @body_monologues
-#      elsif !@name_monologues.blank? and !@body_monologues.blank?
-#        @monologues = @name_monologues & @body_monologues
-#      elsif !@name_monologues.blank? and !@character_monologues.blank?
-#        @monologues = @name_monologues & @character_monologues
-#      end
       @monologues.compact!
       @monologues.uniq!
     else
@@ -95,9 +62,3 @@ class MonologuesController < ApplicationController
     render :partial => 'search', :layout => false
   end
 end
-=begin
-paginate(:page => params[:page],
-                            :per_page => 10,
-                            :order => order_from_params,
-                            :conditions => Monologues.conditions_by_like(params[:search]))
-=end
