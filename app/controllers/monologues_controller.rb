26817a027dcc79f5c1604a5cda53761c2c07b1f1
Brandon Faloona <brandon@faloona.net>
2010-02-02 19:17:32 -0800

    fixed filter

--- app/controllers/monologues_controller.rb
+++ app/controllers/monologues_controller.rb
@@ -48,17 +48,35 @@ class MonologuesController < ApplicationController
   def search
     #require 'ruby-debug';debugger
     unless params[:search].blank?
-      terms = params[:search].split(" ")
-      @name_monologues = []
-      @body_monologues = []
-      @character_monologues = []
-      terms.each do |term|
-        @name_monologues += Monologue.find(:all, :conditions => "name like '%#{term}%'")
-        @body_monologues += Monologue.find(:all, :conditions => "body like '%#{term}%'")
-        @character_monologues += Monologue.find(:all, :conditions => "character like '%#{term}%'")
+      @terms = params[:search].split(" ")
+
+      @monologues = []
+      @terms.each do |term|
+        results = Monologue.find(:all, :conditions => "character like '%#{term}%' or body like '%#{term}%' or name like '%#{term}%'")
+        @monologues = results if results and @monologues.empty?
+        @monologues &= results if results
       end
+#      @name_monologues = []
+#      @body_monologues = []
+#      @character_monologues = []
+#      @terms.each do |term|
+#        @name_monologues += Monologue.find(:all, :conditions => "name like '%#{term}%'")
+#        @body_monologues += Monologue.find(:all, :conditions => "body like '%#{term}%'")
+#        @character_monologues += Monologue.find(:all, :conditions => "character like '%#{term}%'")
+#      end
       # OR
-      @monologues = @name_monologues.flatten + @character_monologues.flatten + @body_monologues.flatten
+#      collections = []
+#      collections << @name_monologues.flatten if !@name_monologues.blank?
+#      collections << @character_monologues.flatten if !@character_monologues.blank?
+#      collections << @body_monologues.flatten if !@body_monologues.blank?
+#      case collections.size
+#      when 1
+#        @monologues = collections[0]
+#      when 2
+#        @monologues = collections[0] & collections[1]
+#      when 3
+#        @monologues = collections[0] & collections[1] & collections[2]
+#      end
       # AND
 #      if !@name_monologues.blank? and !@character_monologues.blank? and !@body_monologues.blank?
 #        @monologues = @name_monologues & @character_monologues & @body_monologues
