80087d61ec6a531358704bb5e25082b70fb9ef3c
Brandon Faloona <brandon@faloona.net>
2010-04-04 22:32:13 -0700

    link to monologue body, or pdf if there is no body.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def link_to_monologue_or_pdf( monologue )
+    if monologue.body
+      link_to( monologue.first_line, monologue )
+    else
+      link_to( monologue.first_line, monologue.pdf_link)
+    end
+  end
 end
