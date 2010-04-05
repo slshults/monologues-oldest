87fe044e3abec0499af00a32b560200f4e799424
Brandon Faloona <brandon@faloona.net>
2010-04-05 06:56:11 -0700

    updated _search to conditionally link using application_helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,27 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
-  def link_to_monologue_or_pdf( monologue )
+  def link_to_monologue( monologue )
     if monologue.body
       link_to( monologue.first_line, monologue )
     else
-      link_to( monologue.first_line, monologue.pdf_link)
+      monologue.first_line
+    end
+  end
+
+  def link_to_monologue_pdf( monologue )
+    if monologue.pdf_link
+      "<a href='#{monologue.pdf_link}'><img title='pdf' alt='pdf' valign='middle' src='/images/print.gif' border='0' /></a>"
+    else
+      "<img title='pdf' alt='pdf' valign='middle' src='/images/print_x.gif' border='0' />"
+    end
+  end
+
+  def link_to_monologue_location( monologue )
+    if monologue.body_link
+      "<a href='#{h monologue.body_link}'>#{h monologue.location}</a>"
+    else
+      h monologue.body_link
     end
   end
 end
