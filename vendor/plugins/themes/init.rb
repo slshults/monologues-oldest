b6db73b686f141206224a24ab0f0f761b95eb47d
Brandon Faloona <brandon@faloona.net>
2010-06-02 07:48:24 -0700

    removed jrails and themes plugins. updated monologues styles. CSSified categories.

--- vendor/plugins/themes/init.rb
+++
@@ -1,4 +0,0 @@
-require File.join(File.dirname(__FILE__), "lib", "theme")
-require File.join(File.dirname(__FILE__), "lib", "flash_helper")
-ActionView::Base.send(:include, Theme::ThemeHelper)
-ActionView::Base.send(:include, FlashHelper)
