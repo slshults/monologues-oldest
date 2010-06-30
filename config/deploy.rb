9ee3edc3e7654a32961b762f5cfbaabc95d68fde
Brandon Faloona <brandon@faloona.net>
2010-06-29 22:16:54 -0700

    capistrano task update + .htaccess

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,15 +3,8 @@ set :domain, "freeinternet.org"
 set :user, "freeinte"
 set :repository,  "git://github.com/bfaloona/monologues.git"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
 set :deploy_to, "/home/freeinte/mono"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
 set :scm, :git
 set :branch, "master"
 set :deploy_via, :remote_cache
@@ -24,6 +17,20 @@ role :db,  domain, :primary => true
 set :use_sudo, false
 set :group_writable, false
 
+desc "Site5 best practice is that set_permissions is a no-op"
 task :set_permissions do
   donothing = true
+end
+
+# Passenger customized tasks
+namespace :deploy do
+  desc "Restarting mod_rails (Passenger) with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails (Passenger)"
+    task t, :roles => :app do ; end
+  end
 end
