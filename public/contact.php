64ccae3db3bf4e0b828027914ba546b4105d81c6
slshults <shakes@shults.org>
2010-07-10 23:43:56 -0700

    added misc files and dirs from old site

---
+++ public/contact.php
@@ -0,0 +1,27 @@
+<HTML>
+<HEAD>
+<TITLE>Contact Shakespeare Monologues Site Admin</TITLE>
+</head>
+<BODY BGCOLOR="#F9F9DF" background=parchment.jpg>
+<table width="625" align="left" valign="top">
+<tr>
+<TD align="right" valign="top" rowspan="2">&nbsp;&nbsp;&nbsp;<a href='http://shakespeare-monologues.org/'><IMG src="http://shakespeare-monologues.org/foliocover.gif" alt="Back to Homepage" title="Back to Homepage" border="0"></a>
+</td></tr>
+
+<TD width="555" align="left" valign="top">
+<FONT face="Geneva, Arial, Helvetica"><FONT size="5" color="#000000" face="Verdana, Arial, Helvetica, sans-serif"><p>&nbsp;<p><B>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;contact</B></FONT><br>
+
+
+<?php
+include_once "sonic_contact.php";
+?>
+<br>
+
+<p>
+
+</td>
+</font>
+
+</BODY>
+</HTML>
+
